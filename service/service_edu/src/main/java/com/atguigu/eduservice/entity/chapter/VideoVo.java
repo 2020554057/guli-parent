2e33a7399073bd79bb6f6aa65f0cecd90e775da9
WuSuJie <2020554057@qq.com>
2023-11-12 22:26:26 +0800

    feat：阿里云视频播放器

--- service/service_edu/src/main/java/com/atguigu/eduservice/entity/chapter/VideoVo.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/entity/chapter/VideoVo.java
@@ -13,6 +13,10 @@ import lombok.Data;
  */
 @Data
 public class VideoVo {
+
     private String id;
+
     private String title;
+
+    private String videoSourceId;//视频id
 }
--- service/service_vod/src/main/java/com/atguigu/vod/controller/VodController.java
+++ service/service_vod/src/main/java/com/atguigu/vod/controller/VodController.java
@@ -2,6 +2,8 @@ package com.atguigu.vod.controller;
 
 import com.aliyuncs.DefaultAcsClient;
 import com.aliyuncs.vod.model.v20170321.DeleteVideoRequest;
+import com.aliyuncs.vod.model.v20170321.GetVideoPlayAuthRequest;
+import com.aliyuncs.vod.model.v20170321.GetVideoPlayAuthResponse;
 import com.atguigu.commonutils.R;
 import com.atguigu.servicebase.exceptionhandler.GuliException;
 import com.atguigu.vod.service.VodService;
@@ -77,6 +79,27 @@ public class VodController {
     public R deleteBatchVideo(@RequestParam("videoIdList") List<String> videoIdList){
         vodService.deleteBatchVideo(videoIdList);
         return R.ok();
+    }
+
+    /**
+     * 根据视频id获取视频凭证
+     */
+    @GetMapping("/getPlayAuth/{id}")
+    public R getPlayAuth(@PathVariable String id) {
+        try {
+            //创建初始化对象
+            DefaultAcsClient client = InitVodClient.initVodClient(ConstantVodUtils.ACCESS_KEY_ID, ConstantVodUtils.ACCESS_KEY_SECRET);
+            //创建获取凭证request和response对象
+            GetVideoPlayAuthRequest request = new GetVideoPlayAuthRequest();
+            //向request设置视频id
+            request.setVideoId(id);
+            //调用方法得到凭证
+            GetVideoPlayAuthResponse response = client.getAcsResponse(request);
+            String playAuth = response.getPlayAuth();
+            return R.ok().data("playAuth", playAuth);
+        } catch (Exception e) {
+            throw new GuliException(20001, "获取凭证失败");
+        }
     }
 
 }
